974ebefae24749d1193d0562b608cf5a88de2378
Corina <corina@fate.org.au>
2024-03-11 11:53:45 +1000

    Route Parameters - Done

--- src/index.mjs
+++ src/index.mjs
@@ -4,16 +4,29 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const mockUsers = [
+  { id: 1, username: "corina", displayName: "Corina" },
+  { id: 2, username: "toro", displayName: "Toro" },
+  { id: 3, username: "mori", displayName: "Mori" },
+]
+
 app.get("/", (request, response) => {
   response.status(201).send({ msg: "Hello" })
 });
 
 app.get('/api/users', (request, response) => {
-  response.send([
-    { id: 1, username: "corina", displayName: "Corina" },
-    { id: 2, username: "toro", displayName: "Toro" },
-    { id: 3, username: "mori", displayName: "Mori" },
-  ])
+  response.send(mockUsers)
+})
+
+app.get('/api/users/:id', (request, response) => {
+  console.log(request.params);
+  const parsedId = parseInt(request.params.id);
+  console.log(parsedId)
+  if (isNaN(parsedId)) return response.setMaxListeners(400).send({ msg: "Bad request. Invalid ID" })
+
+  const findUser = mockUsers.find((user) => user.id === parsedId)
+  if (!findUser) return response.sendStatus(404);
+  return response.send(findUser);
 })
 
 app.get('/api/products', (request, response) => {
